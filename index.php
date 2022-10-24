1b3819ce0f636f0df792a010cad2a8ee200bf683
williampphk <william.pphk@gmail.com>
2022-10-24 10:55:22 +0700

    memanbahkan file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo "hello world!! <br>" ;
+
+$nama = " william pakpahan";
+$umur = 21;
+
+echo " nama saya $nama,  umur $umur tahun <br> ";
+
+$nama_adik = " udin ";
+$umur_adik = 14; 
+
+echo "nama adik saya $nama_adik, dan umurnya $umur_adik ";
+
+$selisih =  $umur - $umur_adik ;
+
+echo " selisih usia kami adalah $selisih ";
+
+?>
