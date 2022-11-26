4a9d6742e53b70f829b4204dea37c19ae3b1afcb
williampphk <william.pphk@gmail.com>
2022-11-26 19:39:10 +0700

    membuat file index dan insert untuk pengolahan database

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php 
+// 1 membuat koneksi
+$koneksi = mysqli_connect("localhost","root","","fakultas");
+ 
+// 2 Check connection
+if (mysqli_connect_errno()){
+	echo "yahh Connect database gagal : " . mysqli_connect_error();
+}
+else { 
+   echo'yeyy berhasil connect'; 
+}
+ // 3. membaca data dari table mysql.
+$sql = "SELECT * FROM mahasiswa";
 
-echo "hello world!! <br>" ;
-
-$nama = " william pakpahan";
-$umur = 21;
-
-echo " nama saya $nama,  umur $umur tahun <br> ";
-
-$nama_adik = " udin ";
-$umur_adik = 14; 
-
-echo "nama adik saya $nama_adik, dan umurnya $umur_adik ";
-
-$selisih =  $umur - $umur_adik ;
-
-echo " selisih usia kami adalah $selisih ";
-
+// 4 tampilkan data cek apakah query bisa berjalan
+$result = mysqli_query ($koneksi, $sql);
+if ($result) {
+    // tampilkan satu-persatu
+    $row = mysqli_fetch_assoc($result) ;
+   
+    while (   $row = mysqli_fetch_assoc($result) ){
+        echo "<br>".$row["nama"]." alamat :".$row ["alamat"];
+    }
+    mysqli_free_result($result);
+}
 ?>
